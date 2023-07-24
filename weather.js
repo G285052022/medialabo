5e512d70ccc79996ea0c34f81bc440546ad349d2
G285052022 <228505@st.takushoku-u.ac.jp>
2023-07-24 22:22:55 +0900

    第6回課題

--- weather.js
+++ weather.js
@@ -45,49 +45,8 @@ let data = {
   "cod": 200
 };
 
-////////// 課題3-2 ここからプログラムを書こう
-/*console.log("緯度:"+data.coord.lon);
-console.log("経度:"+data.coord.lat);
-console.log("天気:"+data.weather[0].description);
-console.log("最低気温:"+data.main.temp_min);
-console.log("最高気温:"+data.main.temp_max);
-console.log("湿度:"+data.main.humidity);
-console.log("風速:"+data.wind.speed);
-console.log("風向:"+data.wind.deg);
-console.log("都市名:"+data.name);
 
-let div=document.querySelector('div#result'); 
-let li = document.createElement('li');
-  li.textContent = "緯度:"+data.coord.lon;
-  div.insertAdjacentElement('beforeend', li);
-  let li1 = document.createElement('li');
-  li1.textContent = "経度:"+data.coord.lat;
-  div.insertAdjacentElement('beforeend', li1);
-  let li2 = document.createElement('li');
-  li2.textContent = "天気:"+data.weather[0].description;
-  div.insertAdjacentElement('beforeend', li2);
-  let li3 = document.createElement('li');
-  li3.textContent = "最低気温:"+data.main.temp_min;
-  div.insertAdjacentElement('beforeend', li3);
-  let li4 = document.createElement('li');
-  li4.textContent = "最高気温:"+data.main.temp_max;
-  div.insertAdjacentElement('beforeend', li4);
-  let li5 = document.createElement('li');
-  li5.textContent = "湿度:"+data.main.humidity;
-  div.insertAdjacentElement('beforeend', li5);
-  let li6 = document.createElement('li');
-  li6.textContent = "風速:"+data.wind.speed;
-  div.insertAdjacentElement('beforeend', li6);
-  let li7 = document.createElement('li');
-  li7.textContent = "風向:"+data.wind.deg;
-  div.insertAdjacentElement('beforeend', li7);
-  let li8 = document.createElement('li');
-  li8.textContent = "都市名:"+data.name;
-  div.insertAdjacentElement('beforeend', li8);
-*/
-
-
-
+let kount=0;
 
 let b2 = document.querySelector('#sendRequest');
 b2.addEventListener('click', sendRequest);
@@ -96,6 +55,13 @@ function sendRequest() {
 	// URL を設定
   let cs = document.querySelectorAll('input[name="wether"]');
   for (let c of cs) {
+    kount=kount+1;
+    if(kount>2){
+      const p1 = document.querySelectorAll("li");
+for(let p2 of p1){
+	p2.remove(); 
+}
+    }
     if(c.checked){
       let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+c.id+'.json';
       axios.get(url)
@@ -150,7 +116,9 @@ let li = document.createElement('li');
   let li8 = document.createElement('li');
   li8.textContent = "都市名:"+data.name;
   div.insertAdjacentElement('beforeend', li8);
-
+  let sp= document.createElement("li");
+  sp.textContent="\n";
+  div.insertAdjacentElement('beforeend', sp);
 }
 
 // 通信エラーが発生した時の処理
@@ -161,4 +129,10 @@ function showError(err) {
 // 通信の最後にいつも実行する処理
 function finish() {
 	console.log('Ajax 通信が終わりました');
+}
+
+
+const p1 = document.querySelectorAll("li");
+for(let p2 of p1){
+	p2.remove(); 
 }
